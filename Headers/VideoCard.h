b4b0834ea7eb121339024dbf9ca77e22fdf74d22
Michael Stefanskyi <98762766+Mihard007@users.noreply.github.com>
2023-05-21 21:51:51 +0300

    Create VideoCard.h

---
+++ Headers/VideoCard.h
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+class VideoCard {
+private:
+	string name;
+	int number;
+
+public:
+	VideoCard() {
+		name = "Nvidia GeForce";
+		number = 3060;
+	}
+
+	VideoCard(string name, int number) {
+		this->name = name;
+		this->number = number;
+	}
+
+	void Show() 
+	{ 
+		cout << "Name: " << name << number << "\n"; 
+	}
+};
